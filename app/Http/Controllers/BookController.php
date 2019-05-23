79d51237cb1ecef847bb3ecc7b68c5ca02bee4e4
Николай Аблов <ablov@a2design.biz>
2019-05-23 17:09:24 +0600

    Fixed index method in BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,9 +8,25 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $books = Book::orderBy('created_at', 'DESC')->paginate(10);
+        $booksQuery = Book::orderBy('created_at', 'DESC')->with('authors');
+
+        $search = request()->search;
+
+        if ($search) {
+            $booksQuery = $booksQuery->where('title', 'like', '%' . $search . '%')
+                ->orWhereHas('authors', function($query) use ($search) {
+                    $query->where('name', 'like', '%' . $search . '%');
+                 });
+        }
+
+        $books = $booksQuery->paginate(10);
 
         return view('books.index', compact('books'));
     }
