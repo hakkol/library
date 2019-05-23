6e42753e4179af0ff4955c0890498c9f78d3408b
Николай Аблов <ablov@a2design.biz>
2019-05-23 08:17:13 +0600

    Added relationships in Author and Book models

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    /**
+    * Get the books associated with the given author.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\belongsToMany
+    */
+    public function books()
+    {
+        return $this->belongsToMany('App\Models\Book', 'author_books')
+            ->orderBy('books.title', 'asc')
+            ->withTimestamps();
+    }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    /**
+    * Get the authors associated with the given book.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\belongsToMany
+    */
+    public function authors()
+    {
+        return $this->belongsToMany('App\Models\Author', 'author_books')
+            ->orderBy('authors.name', 'asc')
+            ->withTimestamps();
+    }
 }
