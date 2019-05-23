deec2e88d9fe39741de25a7a14bb8264b2a8b1b5
Nikolay Ablov <ablov@a2design.biz>
2019-05-23 18:27:24 +0600

    Added Scraper command in commands, fixed schedule method in Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\Scraper::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('scraper')->daily();
     }
 
     /**
