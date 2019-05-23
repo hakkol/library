2e38cf8df6d24bfd862d387d2690ac04588650b7
Николай Аблов <ablov@a2design.biz>
2019-05-23 08:17:29 +0600

    Created author_books table

---
+++ database/migrations/2019_05_23_012856_create_author_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author_books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')
+                  ->references('id')->on('authors')
+                  ->onDelete('cascade');
+
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')
+                  ->references('id')->on('books')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author_books');
+    }
+}
